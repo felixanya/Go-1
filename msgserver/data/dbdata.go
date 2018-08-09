b985e14f88c4249df9e4fa054718eb28e3794d1c
SkyWang <569258817@qq.com>
2018-08-09 12:30:54 +0800

    1.增加日志打印

--- msgserver/data/dbdata.go
+++ msgserver/data/dbdata.go
@@ -54,6 +54,7 @@ func LoadHorseFromDB() (map[int64]*define.HorseRace, error) {
 		horse.IsUseParent = int8(isUseParent)
 		parseHorseJson(horse, row["n_horseData"])
 		list[id] = horse
+		logrus.Debugf("LoadHorseFromDB add one: %v", *horse)
 	}
 
 	logrus.Debugf("LoadHorseFromDB win: sum=%d\n", len(list))
