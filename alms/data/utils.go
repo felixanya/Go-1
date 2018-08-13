c54cc30b5e04f166bc9ad5e2c56b84bb7722c25b
wuhongwei <915168378@qq.com>
2018-08-13 16:47:03 +0800

    救济金修改初步

--- alms/data/utils.go
+++ alms/data/utils.go
@@ -46,33 +46,6 @@ func checkMapStringInterface(m map[string]interface{}, checkString []string) boo
 	return true
 }
 
-// AlmsConfigToMap AlmsConfig to map[string]interface{}
-func AlmsConfigToMap(ac *AlmsConfig) map[string]interface{} {
-	almsConfigMap := make(map[string]interface{})
-	if ac.GetNorm > 0 {
-		almsConfigMap[AlmsGetNorm] = ac.GetNorm // 救济线
-	}
-	if ac.GetTimes > 0 {
-		almsConfigMap[AlmsGetTimes] = ac.GetTimes // 最多领取次数
-	}
-	if ac.GetNumber > 0 {
-		almsConfigMap[AlmsGetNumber] = ac.GetNumber // 领取数量
-	}
-	if ac.AlmsCountDonw > 0 {
-		almsConfigMap[AlmsCountDonw] = ac.AlmsCountDonw // 救济倒计时
-	}
-	if ac.DepositCountDonw > 0 {
-		almsConfigMap[DepositCountDonw] = ac.DepositCountDonw // 快冲倒计时
-	}
-	if len(ac.GameLeveConfigs) > 0 {
-		almsConfigMap[GameLeveConfigs] = GameLeveConfigToJSON(ac.GameLeveConfigs) //游戏场次是否开启救济金
-	}
-	if ac.Version > 0 {
-		almsConfigMap[AlmsVersion] = ac.Version // 版本号
-	}
-	return almsConfigMap
-}
-
 // GameLeveConfigToJSON 游戏场次配置 转 JSON
 func GameLeveConfigToJSON(gemeLeveOK []*GameLeveConfig) string {
 	if gemeLeveOK == nil {
