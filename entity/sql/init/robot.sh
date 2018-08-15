1b133c251943320f358d9788d9bdfb45003f2c6e
杨兆丰 <yangzhaofeng@stevegame.cn>
2018-08-15 14:22:12 +0800

    初始化玩家

--- entity/sql/init/robot.sh
+++ entity/sql/init/robot.sh
@@ -74,7 +74,7 @@ while [ $i -le 50 ]
 do
    echo $i
     mysql -h192.168.9.200 -uroot -p'SDF-XSP-0056' 192_168_9_162_player -e "insert into t_player (playerID,showUID,accountID,type,nickname) values (${i1},${i2},${i3},2,'robot${i1}');"
-    mysql -h192.168.9.200 -uroot -p'SDF-XSP-0056' 192_168_9_162_player -e "insert into t_player_currency (playerID,coins) values (${i1},10000);"
+    mysql -h192.168.9.200 -uroot -p'SDF-XSP-0056' 192_168_9_162_player -e "insert into t_player_currency (playerID,coins) values (${i1},100000);"
     mysql -h192.168.9.200 -uroot -p'SDF-XSP-0056' 192_168_9_162_player -e "insert into t_player_game (playerID,gameID,winningRate) values (${i1},4,50);"
     d=$(date +%M-%d\ %H\:%m\:%S)
     echo "INSERT $i @@ $d"    
