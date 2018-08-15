7c4b095e11694db4a3e92abef38abb5f4f9c2099
zhengxuzhang <zhengxuzhang@stevegame.cn>
2018-08-15 11:29:59 +0800

    Func:1.propserver服加到build.sh脚本 2.hall服调用新的GetConfigUntilSucc方法获取游戏场次配置

--- hall/logic/gameconfig.go
+++ hall/logic/gameconfig.go
@@ -1,9 +1,12 @@
 package logic
 
 import (
-	"context"
+	"encoding/json"
+	"fmt"
 	entityConf "steve/entity/config"
+	"steve/entity/constant"
 	"steve/external/configclient"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 )
@@ -14,41 +17,54 @@ var GameConf []entityConf.GameConfig
 // LevelConf 场次配置
 var LevelConf []entityConf.GameLevelConfig
 
-// InitGameConfig 初始化游戏配置
-func InitGameConfig(ctx context.Context) {
-	var err error
-	for {
-		select {
-		case <-ctx.Done():
-			logrus.Debugf("hall服启动加载的游戏玩法，GameConf:(%v)\n，LevelConf：（%v）", GameConf, LevelConf)
-			return
-		default:
-			GameConf, err = configclient.GetGameConfigMap()
-			if err != nil {
-				continue
-			}
-			LevelConf, err = configclient.GetGameLevelConfigMap()
-			if err != nil {
-				continue
-			}
-			logrus.Debugf("hall服启动加载的游戏玩法，GameConf:(%v)\n，LevelConf：（%v）", GameConf, LevelConf)
-			return
-		}
+// loadGameConfig load game config from configuration server
+func loadGameConfig(retry int) (gameConf []entityConf.GameConfig, err error) {
+	gameConfigJSON, err := configclient.GetConfigUntilSucc(constant.GameConfigKey.Key, constant.GameConfigKey.SubKey, retry, 5*time.Second)
+	if err != nil {
+		return nil, fmt.Errorf("获取game config失败：%s", err.Error())
 	}
+	return parseGameConfig(gameConfigJSON)
 }
 
-// // InitGameConfig 初始化游戏配置
-// func InitGameConfig() error {
-// 	var err error
-// 	// 游戏配置
-// 	GameConf, err = configclient.GetGameConfigMap()
-// 	if err != nil {
-// 		return fmt.Errorf("InitGameConfig 获取游戏配置失败,error:(%v)", err.Error())
-// 	}
-// 	// 场次配置
-// 	LevelConf, err = configclient.GetGameLevelConfigMap()
-// 	if err != nil {
-// 		return fmt.Errorf("InitGameConfig 获取游戏级别配置失败,error:(%v)", err.Error())
-// 	}
-// 	return nil
-// }
+// loadGameLevelConfig load game level config from configuration server
+func loadGameLevelConfig(retry int) (gameConf []entityConf.GameLevelConfig, err error) {
+	gameLevelConfigJSON, err := configclient.GetConfigUntilSucc(constant.GameLevelConfigKey.Key, constant.GameLevelConfigKey.SubKey, retry, 5*time.Second)
+	if err != nil {
+		return nil, fmt.Errorf("获取game level config失败：%s", err.Error())
+	}
+	return parseGameLevelConfig(gameLevelConfigJSON)
+}
+
+// parseGameConfig 解析游戏玩法配置
+func parseGameConfig(config string) (gameConf []entityConf.GameConfig, err error) {
+	if err := json.Unmarshal([]byte(config), &gameConf); err != nil {
+		return nil, fmt.Errorf("反序列化失败：%s", err.Error())
+	}
+	logrus.Debugf("解析游戏玩法配置解析成功: %v", gameConf)
+	return
+}
+
+// parseGameLevelConfig 解析游戏玩法配置
+func parseGameLevelConfig(config string) (gameConf []entityConf.GameLevelConfig, err error) {
+	if err := json.Unmarshal([]byte(config), &gameConf); err != nil {
+		return nil, fmt.Errorf("反序列化失败：%s", err.Error())
+	}
+	logrus.Debugf("解析游戏场次配置解析成功: %v", gameConf)
+	return
+}
+
+// InitGameConfig 初始化游戏配置
+func InitGameConfig() {
+	var err error
+	// 游戏配置
+	GameConf, err = loadGameConfig(20)
+	if err != nil {
+		logrus.WithError(err).Errorln("initGameConfig 获取游戏玩法配置失败")
+	}
+	// 场次配置
+	LevelConf, err = loadGameLevelConfig(20)
+	if err != nil {
+		logrus.WithError(err).Errorln("initGameConfig 获取游戏场次配置失败")
+	}
+	return
+}
